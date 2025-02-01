4abc9540de989c14e140a3d0f6e0a1ca8e132508
Khadim Cisse <khadimcisse00@gmail.com>
2025-02-01 17:34:01 +0100

    correction bug EasyAdmin

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +20,26 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    // /*
-    // public function configureFields(string $pageName): iterable
-    // {
-    //     return [
-    //         IdField::new('id'),
-    //         TextField::new('title'),
-    //         TextEditorField::new('description'),
-    //     ];
-    // }
-    // */
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Utilisateur')
+            ->setEntityLabelInPlural('Utilisateurs')
+        ;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+            //ici je spescifie ce qui peut modifer notre utilisateur admin
+       return [
+
+            TextField::new('firstname')->setLabel('Prenom'),
+            TextField::new('lastname')->setLabel('Nom'),
+            //L'adresse mail n'est pas modifiable mais on peut le voir dans l'index (page utilisateurs)
+            TextField::new('email')->setLabel('Email')->onlyOnIndex(),
+            
+    ];
+    }
+    
 }
